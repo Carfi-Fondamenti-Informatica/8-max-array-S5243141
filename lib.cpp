b77ea5668787549ca78adc84347b96d182302267
S5243141 <99831210+S5243141@users.noreply.github.com>
2022-05-06 17:00:34 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,10 +1,10 @@
 #include "lib.h"
 
 float array(float n[], int x) {
-  float max = n[0];
+  float max=n[0];
   for (int i=1; i<x; i++) {
     if (max < n[i]) {
-      max = n[i];
+      max=n[i];
     }
   } return max;
 }
