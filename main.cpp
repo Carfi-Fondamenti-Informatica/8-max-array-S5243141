6390589e7472c1b0f7624aaff6899e7385a0e214
S5243141 <99831210+S5243141@users.noreply.github.com>
2022-05-06 16:58:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
+int main() {
+  int x=0;
+  cin >> x;
+  float n[x];
+  for (int i=0; i<x; i++) {
+    cin >> n[i];
+  }
+  float y=array(n, x);
+  cout << y <<endl;
   return 0;
 }
